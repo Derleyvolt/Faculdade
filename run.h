6674ab4f724479218bdd7cabd2fb563ea7edbde3
Wanderley <100Palavr@gmail.com>
2021-07-28 14:16:48 -0300

    Delete run.h

--- run.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-#include <cstring>
-#include "KMP.h"
-#include "generate_cases.h"
-#include "brute_force.h"
-
-void run_random(char bound, int pattern_len, int text_len, int instance_len);
-void run_real(int l, int r);
